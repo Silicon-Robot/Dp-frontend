a7e570cd41525d135f7643a0da3a7cfe808a7d6d
Gaetan-M <mopidaryl@gmail.com>
2020-04-04 01:22:51 +0100

    integration backend

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import SendMail from './auth/signIn/pages/SendMail';
+import SignUp from './auth/signUp/pages/SignUp'
+import SignIn from './auth/signIn/pages/SignIn';
+// import Footer from './shared/UIElements/Footer'
+import ForgotPassword from './auth/signIn/pages/ForgotPwd';
+import DisplayTable from './shared/components/DisplayTable';
+
+class App extends React.Component {
+    render() {
+        return (
+         <Router >
+            <Switch >
+            <Route exact path = '/signin' > <SignIn / > </Route>
+            <Route exact path = '/' > <DisplayTable / > </Route> 
+            <Route exact path = '/restpwd' > <ForgotPassword / > </Route> 
+            <Route exact path = '/sendMail' > <SendMail / > </Route> 
+            <Route exact path = '/signup' > <SignUp / > </Route>  
+            <Redirect to = '/' / >hello 
+            </Switch>
+      </Router >
+        );
+    }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './App';
+import rootReducer from './store/reducers/rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers());
+
+
+ReactDOM.render( 
+    <Provider store = { store } >
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
